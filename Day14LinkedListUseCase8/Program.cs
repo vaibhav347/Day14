647bad9f289f4bf8079f505fa612f4179f486e40
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:33:27 +0530

    ok

---
+++ Day14LinkedListUseCase8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day14LinkedListUseCase8
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] num = {56,30,70 };
+            LinkedList<int> list = new LinkedList<int>(num);
+            foreach (var n in list)
+            {
+                Console.WriteLine(n);
+            }
+            // adding a node at the end
+            var newNode = list.AddLast(70);
+            // adding a new node after the node added above
+            list.AddBefore(newNode, 40);
+            Console.WriteLine("LinkedList after adding new nodes...");
+            foreach (var n in list)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
