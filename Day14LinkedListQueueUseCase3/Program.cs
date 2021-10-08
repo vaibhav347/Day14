a64c25aa4f480b384cbdc013ba64bb1b80ec7ee2
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:37:38 +0530

    ok

---
+++ Day14LinkedListQueueUseCase3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day14LinkedListQueueUseCase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase3 obj = new UseCase3();
+            obj.insertData();
+        }
+    }
+}
---
+++ Day14LinkedListQueueUseCase3/UseCase3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14LinkedListQueueUseCase3
+{
+    public class UseCase3
+    {
+        public void insertData()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            Queue st = new Queue();
+            st.Enqueue(list.AddFirst(70));
+            st.Enqueue(list.AddFirst(30));
+            st.Enqueue(list.AddFirst(56));
+            foreach (int element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
