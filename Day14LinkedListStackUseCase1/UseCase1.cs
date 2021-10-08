c45622a019377fd2ab4145dae53189bcbb332cd5
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:35:42 +0530

    ok

---
+++ Day14LinkedListStackUseCase1/UseCase1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14StackQueueUseCase1
+{
+    public class UseCase1
+    {
+        public void insertData()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            Stack st = new Stack();
+            st.Push(list.AddFirst(70));
+            st.Push(list.AddFirst(30));
+            st.Push(list.AddFirst(56));
+            foreach (int element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
