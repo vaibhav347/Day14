3f6acb20c86166677d3108df1ee731cafb7da51d
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:36:18 +0530

    ok

---
+++ Day14LinkedListStackUseCase2/UseCase2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14StackQueueUseCase2
+{
+    public class UseCase2
+    {
+        public void insertData()
+        {
+            //LinkedList<int> list = new LinkedList<int>();
+            Stack st = new Stack();
+            st.Push(70);
+            st.Push(30);
+            st.Push(56);
+            foreach (int element in st)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine("Elements After Pop");
+            st.Pop();
+            foreach (int element in st)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("------------");
+            st.Pop();
+            foreach (int element in st)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("------------");
+            st.Pop();
+            foreach (int element in st)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
