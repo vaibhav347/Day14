1abdd0c1964f99a7709823c7fdad2f14ca1846dd
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:28:23 +0530

    ok

---
+++ Day14LinkedListUseCase3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day14LinkedListUseCase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase3 obj = new UseCase3();
+            obj.insertData();
+
+        }
+    }
+}
