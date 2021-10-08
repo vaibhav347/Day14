97b3bedb11e829b2fbbf748081fb617b1118ebf0
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:28:50 +0530

    ok

---
+++ Day14LinkedListUseCase4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day14LinkedListUseCase4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase4 obj = new UseCase4();
+            obj.insertData();
+        }
+    }
+}
---
+++ Day14LinkedListUseCase4/UseCase4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14LinkedListUseCase4
+{
+    public class UseCase4
+    {
+        public void insertData()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(70);
+            list.AddFirst(30);
+            list.AddFirst(56);
+            foreach (int element in list)
+             Console.Write(element+"-->");
+        }
+    }
+}
