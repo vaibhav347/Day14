348bebba81b9b1e6825fd20f25851258d1eee65b
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:33:48 +0530

    ok

---
+++ Day14LinkedListUseCase9/UseCase9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14LinkedListUseCase9
+{
+    public class UseCase9
+    {
+        public void insertData()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(70);
+            list.AddFirst(40);
+            list.AddFirst(30);
+            list.AddFirst(56);
+            foreach (int data in list)
+                Console.Write(data + "-->");
+            list.Remove(40);
+            Console.WriteLine();
+            Console.WriteLine("---------------");
+            Console.WriteLine("List After Delete Node 40");
+            foreach (int data in list)
+                Console.Write(data + "-->");
+            Console.WriteLine();
+            Console.WriteLine("---------------");
+            int a = list.Count;
+            Console.WriteLine("Size : " + a);
+
+
+
+        }
+    }
+}
