3606965da2f189f081f3cbee92cea49a37ef98e7
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:33:07 +0530

    ok

---
+++ Day14LinkedListUseCase7/UseCase7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14LinkedListUseCase7
+{
+    public class UseCase7
+    {
+        public void insertData()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(70);
+            list.AddFirst(30);
+            list.AddFirst(56);
+            foreach (int element in list)
+                Console.Write(element + "-->");
+            Console.WriteLine();
+            foreach(int element in list)
+            {
+                if(element == 30)
+                {
+                    Console.WriteLine("Fount");
+                }
+            }
+
+            
+        }
+    }
+}
