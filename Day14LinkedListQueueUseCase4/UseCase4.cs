2dc884d71327286f034dfad2ebeff000a6f4a57d
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:38:03 +0530

    ok

---
+++ Day14LinkedListQueueUseCase4/UseCase4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14LinkedListQueueUseCase4
+{
+    public class UseCase4
+    {
+        public void insertData()
+        {
+           // LinkedList<int> list = new LinkedList<int>();
+            Queue st = new Queue();
+            st.Enqueue(70);
+            st.Enqueue(30);
+            st.Enqueue(56);
+            foreach (int element in st)
+            {
+                Console.WriteLine(element);
+            }
+            st.Dequeue();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Dequeue");
+            foreach (int element in st)
+            {
+                Console.WriteLine(element);
+            }
+            st.Dequeue();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Dequeue");
+            foreach (int element in st)
+            {
+                Console.WriteLine(element);
+            }
+            st.Dequeue();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Dequeue");
+            foreach (int element in st)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
